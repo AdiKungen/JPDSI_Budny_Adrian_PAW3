324a2edffa715c41c4d3065639214b0654f2beb7
a_budny <127764960+AdiKungen@users.noreply.github.com>
2023-03-28 00:08:03 +0200

    Add files via upload

---
+++ budny_zad2_ochrona_zasobow/app/calc.php
@@ -0,0 +1,76 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+include _ROOT_PATH.'/app/security/check.php';
+
+function getParams(&$kwo, &$lat, &$opr) {
+    $kwo = isset($_REQUEST['kwo']) ? $_REQUEST['kwo'] : null;
+    $lat = isset($_REQUEST['lat']) ? $_REQUEST['lat'] : null;
+    $opr = isset($_REQUEST['opr']) ? $_REQUEST['opr'] : null;
+}
+
+function validate(&$kwo, &$lat, &$opr, &$messages) {
+
+    if (!(isset($kwo) && isset($lat) && isset($opr))) {
+	    return false;
+    }
+
+    if ($kwo == "") {
+        $messages [] = 'Nie podano kwoty kredytu';
+    } else if (!is_numeric($kwo)) {
+	    $messages [] = 'Kwota nie jest wartością numeryczną';
+	} else if($kwo <= 0) {
+        $messages [] = 'Kwota nie może być wartością niedodatnią';
+    }
+
+	if ($lat == "") {
+        $messages [] = 'Nie podano ilości lat';
+    } else if (!is_numeric($lat)) {
+	    $messages [] = 'Ilość lat nie jest wartością numeryczną';
+	} else if($lat <= 0) {
+        $messages [] = 'Ilość lat nie może być wartością niedodatnią';
+    }
+
+    if ($opr == "") {
+        $messages [] = 'Nie podano oprocentowania';
+    } else if (!is_numeric($opr)) {
+	    $messages [] = 'Oprocentowanie nie jest wartością numeryczną';
+	} else if($opr < 0) {
+        $messages [] = 'Oprocentowanie nie może być wartością ujemną';
+    }
+
+    if (count($messages) != 0) return false;
+	else return true;
+}
+
+function process(&$kwo,&$lat,&$opr,&$messages,&$result) {
+	global $rola;
+
+    $kwo = intval($kwo);
+    $lat = intval($lat);
+    $opr = intval($opr);
+
+    if($rola == "użytkownik") {
+        if($kwo > 10000) {
+            $messages [] = "Tylko administrator może brać kredyt większy niż 10000";
+        } else {
+            $result = max(round(($kwo + ($kwo * ($opr/100))) / ($lat * 12), 2), 0.01);
+        }
+    } else {
+        $result = max(round(($kwo + ($kwo * ($opr/100))) / ($lat * 12), 2), 0.01);
+    }
+    
+}
+
+$kwo = null;
+$lat = null;
+$opr = null;
+$result = null;
+$messages = array();
+
+getParams($kwo, $lat, $opr);
+if(validate($kwo, $lat, $opr, $messages)) {
+    process($kwo,$lat,$opr,$messages,$result);
+}
+
+include 'calc_view.php';
